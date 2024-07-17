33a3cdf165145d671f6a3d5eb69be9393028e4ee
mumladze1 <141115595+mumladze1@users.noreply.github.com>
2024-07-17 14:53:41 +0400

    add

---
+++ davaleba1.2/main.js
@@ -0,0 +1,64 @@
+
+// davaleba 1
+
+// function compare(a, b) {
+//     if (a === b) {
+//         return "tolia";
+//     } else {
+//         return "ar aris toli";
+//     }
+// }
+
+
+
+
+
+
+// let result1 = compare(2, 7);
+// console.log(result1); 
+
+
+// let result2 = compare(5, 5);
+// console.log(result2); 
+
+
+
+
+
+
+
+
+// davaleba 2
+
+
+// function tem( Fahrenheit,Celsius){ 
+//     if(typeof Fahrenheit==='string' ) {return false}
+//     if(typeof Celsius ==='string' ) {return false}
+//     return Celsius
+    
+
+
+
+// }
+
+
+// console.log(tem(1,180))
+// console.log(tem('dd',180))
+
+
+
+
+
+// davaleba 3
+
+
+// function calculate(num1,num2) {
+// if(typeof num1==='string'){ return false}
+// if(typeof num2==='string'){ return false}
+// return num1+num2
+
+// }
+
+// console.log( calculate(10,40))
+// console.log( calculate("dm",40))
+// console.log( calculate("20",40))
